d72dde988787466249a3123011705c9132870fd3
Nick Stafford <nicholas.stafford@webappuk.com>
2015-01-09 15:39:06 +0000

    Initial commit of files carried over from previous iteration of the repo. Includes Bootstrap but no JQuery yet. Also including a .gitignore

---
+++ Playground/js/canvascontrolbuttons.js
@@ -0,0 +1,41 @@
+﻿function fillTestRed() {
+          var target = document.getElementById("testCanvas");
+          var targetContext = target.getContext("2d");
+          
+          var height = target.height;
+          var width = target.width;
+          
+          targetContext.fillStyle = "#FF0001";
+          targetContext.fillRect(0, 0, width, height);
+}
+
+function clearCanvas() {      
+          var target = document.getElementById("testCanvas");
+          var targetContext = target.getContext("2d");
+          
+          var height = target.height;
+          var width = target.width;
+          
+          targetContext.clearRect( 0, 0, width, height);
+}
+
+function codeSubmit(target, textbox) {
+          codeInsert(target, textbox);
+}
+
+function codeInsert(target, textbox) {
+          
+          var checktype = document.getElementById(target).tagName;
+          var code = document.getElementById(textbox).value;
+          
+          
+          code = '<script type="text/javascript">' + code + '</script>';
+
+          if(checktype === "CANVAS") {
+                    // Apply the canvas code here.
+                    
+                    MSApp.execUnsafeLocalFunction(function () {
+                              document.getElementById('reactiveScriptDiv').innerHTML = code;
+                    });
+          }
+}
---
+++ Playground/js/mainpagebuilder.js
@@ -0,0 +1,16 @@
+﻿function populateMain() {
+          var msg = new Windows.UI.Popups.MessageDialog("Tested");
+       //   msg.showAsync();
+
+          var msg = new Windows.UI.Popups.MessageDialog("Some error has occurred.");
+          
+          buildTabs(msg);
+};
+
+function buildTabs(msgBox) {
+          if (msgBox == typeof Windows.UI.Popups.MessageDialog) {
+                    msgBox.Content = "buildTabs Tested";
+          }
+
+          msgBox.showAsync();
+};
